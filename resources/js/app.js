a71d9b74eecb5227da8bc3f11912ee073383e9e1
codes.kenjimagto <codes.kenjimagto@gmail.com>
2020-09-15 21:59:30 +0800

    Fixing HTTPS issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -33,9 +33,7 @@ import mixins from './utils/mixins'
 import App from './views/App'
 
 window.TOKEN_KEY = 'KX-HEXA'
-window.API_URL = (window.location.host === 'suite.hexa.ph') ?  'https://'+ window.location.host +'/api' : '//'+ window.location.host +'/api'
-
-console.log(API_URL)
+window.API_URL = 'https://'+ window.location.host +'/api'
 
 Vue.use(datePicker)
 Vue.use(Vuex) 
