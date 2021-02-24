0ad04500865d2769c448a5b0089ea167ee8bc5a5
John <johnrobin.barrientos@gmail.com>
2021-02-24 19:33:15 +0800

    changes in tabs

--- resources/js/views/client/buy-and-pay/orders/main.vue
+++ resources/js/views/client/buy-and-pay/orders/main.vue
@@ -1,21 +1,30 @@
 <template>
     <div>
-        <div class="hx-tab">
-
-            <ul class="nav nav-tabs">    
+        <div class="hx-tab-1 round">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">    
                 <li class="nav-item">        
-                    <a class="nav-link active" data-toggle="tab" href="#purchase-list">Purchase Order</a>    
-                </li>    
+                    <a class="nav-link" data-toggle="tab" href="#purchase-reason-code" role="tab">
+                        <span class="d-block d-sm-none"></span>
+                        <span class="d-none d-sm-block">Reason Code</span>    
+                    </a>     
+                </li>
                 <li class="nav-item">        
-                    <a class="nav-link" data-toggle="tab" href="#purchase-reason-code">Reason Code</a>    
-                </li>    
+                    <a class="nav-link active" data-toggle="tab" href="#purchase-list" role="tab">
+                        <span class="d-block d-sm-none"></span>
+                        <span class="d-none d-sm-block">Purchase Order</span>    
+                    </a>   
+                </li>  
             </ul>
 
+            <div class="clearfix"></div>
+            <!-- Tab panes -->
+
             <div class="tab-content">    
-                <div class="tab-pane active" id="purchase-list">        
+                <div class="tab-pane active" id="purchase-list" role="tabpanel">        
                     <list></list>   
                 </div>    
-                <div class="tab-pane" id="purchase-reason-code">        
+                <div class="tab-pane" id="purchase-reason-code" role="tabpanel">        
                     <reason-codes></reason-codes>    
                 </div>    
             </div>
